89c8f3de280d1d7acdb46628ba3dbfa066024523
Mark <mark@smartmile.eu>
2020-11-18 10:03:17 +0300

    Styled individual pages with Tabs for easier navigation

--- lib/screens/home/employment_screen.dart
+++ lib/screens/home/employment_screen.dart
@@ -3,22 +3,53 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EmploymentScreen extends StatelessWidget {
+  Widget _tabHeaders() {
+    return TabBar(
+      tabs: [
+        TabWidget(title: 'Permanent'),
+        TabWidget(title: 'Contract'),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).accentColor,
-        title: Text(
-          'Employment Opportunities',
-          style: Theme.of(context).textTheme.headline3,
-        ),
-        actions: [
-          CircleButton(
-            icon: CupertinoIcons.search,
-            onPressed: () => print('Search'),
-            tooltip: 'Search',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).accentColor,
+          title: Text(
+            'Employment Opportunities',
+            style: Theme.of(context).textTheme.headline3,
           ),
-        ],
+          actions: [
+            CircleButton(
+              icon: CupertinoIcons.search,
+              onPressed: () => print('Search'),
+              tooltip: 'Search',
+            ),
+          ],
+          bottom: _tabHeaders(),
+        ),
+        body: TabBarView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                "Permanent",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                "Contract",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -5,3 +5,4 @@ export 'search_field.dart';
 export 'doctors_list.dart';
 export 'institutions_list.dart';
 export 'app_drawer.dart';
+export 'tab_widget.dart';
