bf686a12289b8f1998b6082501bc53e3b8895e3c
Mark <mcarlton33@gmail.com>
2020-11-27 20:16:28 +0300

    modularized and bundled medical services

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,8 @@
 export 'home_screen.dart';
-export 'services_screen.dart';
 export 'medical_facilities/facilities.dart';
 export 'concierge_screen.dart';
 export 'store_screen.dart';
 export 'resources_screen.dart';
 export 'patient_center_screen.dart';
 export 'emploment_opportunities/employment.dart';
+export 'medical_services/services.dart';
---
+++ lib/screens/home/medical_services/services.dart
@@ -0,0 +1,6 @@
+export 'services_screen.dart';
+export 'preventive_services.dart';
+export 'rehabilitative_services.dart';
+export 'therapeutic_services.dart';
+export 'wellness_services.dart';
+export 'diagnostic_services.dart';
