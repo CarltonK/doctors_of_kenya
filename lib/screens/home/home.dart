e791e793aa068a26a684c96b78f607988930ce6d
Mark <mcarlton33@gmail.com>
2020-11-27 20:36:19 +0300

    modularized and bundled medical concierge and medical store

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,7 @@
 export 'home_screen.dart';
 export 'medical_facilities/facilities.dart';
-export 'concierge_screen.dart';
-export 'store_screen.dart';
+export 'medical_concierge/concierge.dart';
+export 'medical_store/store.dart';
 export 'resources_screen.dart';
 export 'patient_center_screen.dart';
 export 'emploment_opportunities/employment.dart';
