d63807f362cb43772ce396780dbcaef96f21645c
Mark <mcarlton33@gmail.com>
2020-12-13 11:33:50 +0300

    store item view

--- lib/screens/home/medical_store/attire_store.dart
+++ lib/screens/home/medical_store/attire_store.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:doctors_of_kenya/widgets/widgets.dart';
 
 class AttireStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          StoreItem(),
+          StoreItem(),
+          StoreItem(),
+          StoreItem(),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home/medical_store/equipment_store.dart
+++ lib/screens/home/medical_store/equipment_store.dart
@@ -1,8 +1,20 @@
+import 'package:doctors_of_kenya/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class EquipmentStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          StoreItem(),
+          StoreItem(),
+          StoreItem(),
+          StoreItem(),
+        ],
+      ),
+    );
   }
 }
