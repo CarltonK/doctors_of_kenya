0110fc9b94d6aa8734f3323c9dfad73ffc2f0919
Mark <mcarlton33@gmail.com>
2021-03-24 08:23:06 +0100

    added firestore document addition functionality

--- lib/providers/database_provider.dart
+++ lib/providers/database_provider.dart
@@ -1,1 +1,20 @@
-class DatabaseProvider {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doctors_of_kenya/models/models.dart';
+
+class DatabaseProvider {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  DatabaseProvider() {
+    print('Firestore has been initialized.');
+  }
+
+  Future saveUser(UserModel user, String uid) async {
+    user.uid = uid;
+    // Main Doc
+    DocumentReference mainDocRef = _db.collection("users").doc(uid);
+
+    // Save the email to the main document
+    await mainDocRef.set(user.toMainFirestoreDoc());
+    // Create a public document
+  }
+}
