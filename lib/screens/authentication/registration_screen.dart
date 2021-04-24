bf7605170fe9706eb5471e14dfec3e93486989cc
Mark <mcarlton33@gmail.com>
2021-04-24 09:28:21 +0200

    base stepper implemented

--- lib/screens/authentication/registration_screen.dart
+++ lib/screens/authentication/registration_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:doctors_of_kenya/constants/constants.dart';
+// import 'package:doctors_of_kenya/constants/constants.dart';
 import 'package:doctors_of_kenya/helpers/helpers.dart';
 import 'package:doctors_of_kenya/models/models.dart';
 import 'package:doctors_of_kenya/providers/providers.dart';
@@ -23,7 +23,7 @@ class RegistrationScreen extends StatelessWidget {
             CustomPaint(
               painter: BackgroundPainter(),
             ),
-            RegistrationBody(),
+            RegistrationStepper(),
           ],
         ),
       ),
@@ -31,32 +31,52 @@ class RegistrationScreen extends StatelessWidget {
   }
 }
 
-class RegistrationBody extends StatelessWidget {
+class RegistrationStepper extends StatefulWidget {
+  @override
+  _RegistrationStepperState createState() => _RegistrationStepperState();
+}
+
+class _RegistrationStepperState extends State<RegistrationStepper> {
+  // Identifiers
+  int _currentStep = 0;
+  StepperType _stepperType = StepperType.vertical;
+  String _email;
+  String _password;
+  String _confirmPassword;
+  UserModel _userModel;
+  dynamic _registrationResult;
+
+  // Focus Nodes
+  final FocusNode _focusPassword = FocusNode();
+  final FocusNode _focusConfirmPassword = FocusNode();
+
   // Form
   final _registrationFormKey = GlobalKey<FormState>();
 
-  // Focus Nodes
-  final _focusPassword = FocusNode();
-  final _focusConfirmPassword = FocusNode();
+  // TextControllers
+  TextEditingController passwordMain;
+  TextEditingController passwordSecondary;
 
   // Validation
-  static ValidationHelper _validationHelper = ValidationHelper.empty();
+  ValidationHelper _validationHelper = ValidationHelper.empty();
 
-  // TextControllers
-  static TextEditingController passwordMain = TextEditingController();
-  static TextEditingController passwordSecondary = TextEditingController();
+  tapped(int step) {
+    setState(() => _currentStep = step);
+  }
 
-  // Identifiers
-  static String _email;
-  static String _password;
-  static String _confirmPassword;
-  static UserModel _userModel;
-  static dynamic _registrationResult;
+  continued() {
+    // ignore: unnecessary_statements
+    _currentStep < 2 ? setState(() => _currentStep += 1) : null;
+  }
+
+  cancel() {
+    // ignore: unnecessary_statements
+    _currentStep > 0 ? setState(() => _currentStep -= 1) : null;
+  }
 
   // ******Email Stuff*******
-  Widget _emailField(BuildContext context) {
+  Widget _emailField() {
     return Container(
-      padding: const EdgeInsets.all(6),
       child: TextFormField(
         textInputAction: TextInputAction.next,
         decoration: InputDecoration(
@@ -78,9 +98,8 @@ class RegistrationBody extends StatelessWidget {
   }
 
   // ******Password Stuff*******
-  Widget _passwordField(BuildContext context) {
+  Widget _passwordField() {
     return Container(
-      padding: const EdgeInsets.all(6),
       child: TextFormField(
         controller: passwordMain,
         textInputAction: TextInputAction.next,
@@ -104,14 +123,13 @@ class RegistrationBody extends StatelessWidget {
   }
 
   // ******Password Confirm Stuff*******
-  Widget _passwordConfirmField(BuildContext context) {
+  Widget _passwordConfirmField() {
     return Container(
-      padding: const EdgeInsets.all(6),
       child: TextFormField(
         controller: passwordSecondary,
         textInputAction: TextInputAction.done,
         decoration: InputDecoration(
-          labelText: 'Password',
+          labelText: 'Confirm password',
           prefixIcon: const Icon(Icons.vpn_key),
         ),
         validator: (value) {
@@ -123,7 +141,7 @@ class RegistrationBody extends StatelessWidget {
         focusNode: _focusConfirmPassword,
         onFieldSubmitted: (String value) {
           FocusScope.of(context).unfocus();
-          _registrationButtonPressed(context);
+          _registrationButtonPressed();
         },
         onSaved: saveConfirmPassword,
       ),
@@ -135,7 +153,7 @@ class RegistrationBody extends StatelessWidget {
     // print('Password (2) -> $_confirmPassword');
   }
 
-  Future<bool> _regHandler(BuildContext context, UserModel user) async {
+  Future<bool> _regHandler(UserModel user) async {
     _registrationResult = await Provider.of<AuthProvider>(
       context,
       listen: false,
@@ -147,7 +165,7 @@ class RegistrationBody extends StatelessWidget {
     return true;
   }
 
-  _registrationButtonPressed(BuildContext context) {
+  _registrationButtonPressed() {
     final FormState form = _registrationFormKey.currentState;
     if (form.validate()) {
       form.save();
@@ -158,7 +176,7 @@ class RegistrationBody extends StatelessWidget {
         password: _password == _confirmPassword ? _confirmPassword : null,
       );
 
-      _regHandler(context, _userModel).then((value) {
+      _regHandler(_userModel).then((value) {
         if (!value) {
           Timer(Duration(milliseconds: 500), () async {
             await showInfoDialog(
@@ -181,71 +199,161 @@ class RegistrationBody extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    passwordMain = TextEditingController();
+    passwordSecondary = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    passwordMain.dispose();
+    passwordSecondary.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: double.infinity,
-      child: SingleChildScrollView(
-        physics: AlwaysScrollableScrollPhysics(),
-        padding: layoutPadding(context),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 100),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FlutterLogo(size: 100),
-              const SizedBox(height: 16),
-              Card(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 100, top: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Card(
                 margin: const EdgeInsets.symmetric(horizontal: 16),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(4),
                 ),
                 elevation: 3,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 24,
-                  ),
-                  child: Form(
-                    key: _registrationFormKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'Welcome',
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        const SizedBox(height: 3),
-                        Text(
-                          'Create an Account',
-                          style: Theme.of(context).textTheme.subtitle1,
-                        ),
-                        const SizedBox(height: 16),
-                        _emailField(context),
-                        const SizedBox(height: 16),
-                        _passwordField(context),
-                        const SizedBox(height: 16),
-                        _passwordConfirmField(context),
-                        const SizedBox(height: 16),
-                        GlobalActionButton(
-                          action: 'Register',
-                          onPressed: () => _registrationButtonPressed(context),
-                        ),
-                      ],
+                child: Stepper(
+                  steps: <Step>[
+                    Step(
+                      title: Text(
+                        'Account',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      content: Column(
+                        children: [
+                          _emailField(),
+                          _passwordField(),
+                          _passwordConfirmField(),
+                        ],
+                      ),
+                      isActive: _currentStep >= 0,
+                      state: _currentStep >= 1
+                          ? StepState.complete
+                          : StepState.disabled,
                     ),
-                  ),
+                    Step(
+                      title: Text(
+                        'Account',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      content: Column(
+                        children: [
+                          _emailField(),
+                          _passwordField(),
+                          _passwordConfirmField(),
+                        ],
+                      ),
+                      isActive: _currentStep >= 0,
+                      state: _currentStep >= 1
+                          ? StepState.complete
+                          : StepState.disabled,
+                    ),
+                  ],
+                  type: _stepperType,
+                  currentStep: _currentStep,
+                  onStepTapped: (step) => tapped(step),
+                  onStepContinue: continued,
+                  onStepCancel: cancel,
                 ),
               ),
-              const SizedBox(height: 16),
-              NavigationHelper(
-                leading: "Already have an account?",
-                action: "Login",
-                onTap: () => Navigator.of(context).pop(),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 16),
+            NavigationHelper(
+              leading: "Already have an account?",
+              action: "Login",
+              onTap: () => Navigator.of(context).pop(),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+// class RegistrationBody extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: double.infinity,
+//       child: SingleChildScrollView(
+//         physics: AlwaysScrollableScrollPhysics(),
+//         padding: layoutPadding(context),
+//         child: Padding(
+//           padding: EdgeInsets.symmetric(vertical: 100),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               FlutterLogo(size: 100),
+//               const SizedBox(height: 16),
+//               Card(
+//                 margin: const EdgeInsets.symmetric(horizontal: 16),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(4),
+//                 ),
+//                 elevation: 3,
+//                 child: Container(
+//                   padding: const EdgeInsets.symmetric(
+//                     horizontal: 16,
+//                     vertical: 24,
+//                   ),
+//                   child: Form(
+//                     key: _registrationFormKey,
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       mainAxisSize: MainAxisSize.min,
+//                       children: [
+//                         Text(
+//                           'Welcome',
+//                           style: Theme.of(context).textTheme.headline5,
+//                         ),
+//                         const SizedBox(height: 3),
+//                         Text(
+//                           'Create an Account',
+//                           style: Theme.of(context).textTheme.subtitle1,
+//                         ),
+//                         const SizedBox(height: 16),
+//                         _emailField(context),
+//                         const SizedBox(height: 16),
+//                         _passwordField(context),
+//                         const SizedBox(height: 16),
+//                         _passwordConfirmField(context),
+//                         const SizedBox(height: 16),
+//                         GlobalActionButton(
+//                           action: 'Register',
+//                           onPressed: () => _registrationButtonPressed(context),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 16),
+//               NavigationHelper(
+//                 leading: "Already have an account?",
+//                 action: "Login",
+//                 onTap: () => Navigator.of(context).pop(),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
