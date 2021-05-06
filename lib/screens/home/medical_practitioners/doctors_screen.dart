f043af5e9316c1d4aeaa81c4230775a295736ae9
Mark Carlton <mcarlton33@gmail.com>
2021-05-06 08:31:27 +0200

    populate doctor screen

--- lib/screens/home/medical_practitioners/doctors_screen.dart
+++ lib/screens/home/medical_practitioners/doctors_screen.dart
@@ -1,18 +1,34 @@
+import 'package:doctors_of_kenya/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:doctors_of_kenya/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class DoctorsScreen extends StatelessWidget {
+  final String type = 'Doctor';
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-        children: [
-          PractitionerCard(),
-          PractitionerCard(),
-          PractitionerCard(),
-          PractitionerCard(),
-        ],
+      child: FutureBuilder(
+        future: context.watch<DatabaseProvider>().retrievePractitioners(type),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return GlobalErrorContained(
+              errorMessage: '${snapshot.error.toString()}',
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            print(snapshot.data);
+            return ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) => PractitionerCard(),
+            );
+          }
+
+          return GlobalLoader();
+        },
       ),
     );
   }
