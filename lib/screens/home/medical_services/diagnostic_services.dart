4f2a49cdb17f922a8af485a68259d9c8b8a100e1
Mark Carlton <mcarlton33@gmail.com>
2021-05-10 07:10:02 +0200

    consume facility services data

--- lib/screens/home/medical_services/diagnostic_services.dart
+++ lib/screens/home/medical_services/diagnostic_services.dart
@@ -1,8 +1,12 @@
+import 'package:doctors_of_kenya/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class DiagnosticServices extends StatelessWidget {
+  final String type = 'diagnostic';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ServiceRetrieval(type: type),
+    );
   }
 }
--- lib/screens/home/medical_services/rehabilitative_services.dart
+++ lib/screens/home/medical_services/rehabilitative_services.dart
@@ -1,8 +1,12 @@
+import 'package:doctors_of_kenya/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class RehabiilitativeServices extends StatelessWidget {
+  final String type = 'rehabilitative';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ServiceRetrieval(type: type),
+    );
   }
 }
