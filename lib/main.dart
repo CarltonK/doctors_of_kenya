9337723095f21dabb1defdd9c7d711882b62a699
Mark Carlton <mcarlton33@gmail.com>
2021-06-21 08:43:07 +0300

    change application name

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Doctors of Kenya',
+      title: 'Dok',
       debugShowCheckedModeBanner: false,
       navigatorObservers: <NavigatorObserver>[observer],
       theme: ThemeData(
