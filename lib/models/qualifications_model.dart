6f70c29268c7a9a2a9aa101f58fa6a27e67426d5
Mark Carlton <mcarlton33@gmail.com>
2021-08-01 21:22:43 +0300

    Added user properties

--- lib/models/qualifications_model.dart
+++ lib/models/qualifications_model.dart
@@ -1,34 +1,18 @@
 import 'package:doctors_of_kenya/models/models.dart';
 
 class QualificationsModel {
-  List<String> undergraduateDegree;
-  List<String> internshipCentre;
-  List<String> postGraduateDegree;
-  List<String> fellowship;
-  List<String> specializations;
-  List<String> shortCourses;
-  List<String> specialInterests;
+  List<String> qualifications;
+  String? qualificationYear;
   List<FacilityModel> affiliatedInstitutions;
 
   QualificationsModel({
-    this.undergraduateDegree = const <String>[],
-    this.internshipCentre = const <String>[],
-    this.postGraduateDegree = const <String>[],
-    this.fellowship = const <String>[],
-    this.specializations = const <String>[],
-    this.shortCourses = const <String>[],
-    this.specialInterests = const <String>[],
+    this.qualifications = const <String>[],
+    this.qualificationYear,
     this.affiliatedInstitutions = const <FacilityModel>[],
   });
 
   Map<String, dynamic> toJson() => {
-        'undergraduateDegree': undergraduateDegree,
-        'internshipCentre': internshipCentre,
-        'postGraduateDegree': postGraduateDegree,
-        'fellowship': fellowship,
-        'specializations': specializations,
-        'shortCourses': shortCourses,
-        'specialInterests': specialInterests,
+        'undergraduateDegree': qualifications,
         'affiliatedInstitutions': affiliatedInstitutions,
       };
 }
